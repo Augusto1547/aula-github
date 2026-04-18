815b38fc65e4bd6b7c81174e6e207f76f9084b74
Augusto1547 <negocios.augusto@gmail.com>
2026-04-18 13:23:52 -0300

    Inserção de registro na tabela

--- script.sql
+++ script.sql
@@ -4,6 +4,9 @@ create table tb_alunos (
     id int auto_increment primary key,
     nome varchar(50) not null,
     email varchar(150) not null,
-    dataNascimento datetime not
+    dataNascimento datetime not null
 
 );
+
+insert into treina_recife.tb_alunos ('id', 'nome', 'email', 'data_nascimento')
+values (1, "Alexandre de Souza Jr.", "alexandre@treinarecife.com.br", timestamp '1995-07-28');
